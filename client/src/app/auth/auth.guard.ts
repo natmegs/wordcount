a3e87b34cf4aacffeef73b1fffc30e2b1360fa4b
natmegs <nataliemegans@gmail.com>
2018-06-02 17:03:45 -0700

    fix auth guard

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -1,16 +1,24 @@
 import { Injectable } from "@angular/core";
-import { CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivateChild {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
  
   canActivateChild(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean>|Promise<boolean>|boolean {
-    return this.authService.isAuthenticated();
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      this.router.navigate(['/register']);
+      return false;
+    }
   }
 }
